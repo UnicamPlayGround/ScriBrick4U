5efbd1158150d983b5b3376b276072034e17b310
Matias <69719447+ShawQQ@users.noreply.github.com>
2022-07-10 21:06:19 +0200

    Created project template (#1)
    * solution file
    
    * updated gitignore
    
    * init backend
    
    * init BackendTest
    
    * init FrontendTest
    
    * init frontend

---
+++ Backend/Blocks/AbstractBlock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Blocks
+{
+    internal class AbstractBlock : IBlock
+    {
+        public string Type { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public List<IBlock>? Children { get; set; }
+        public IBlock? Next { get; set; }
+        public IBlock? Prev { get; set; }
+    }
+}
