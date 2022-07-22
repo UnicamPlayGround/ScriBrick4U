b6c195a68a75d4e1898a3465a2b022973139f739
Giacomo <giacomo.matteucci@studenti.unicam.it>
2022-07-22 09:18:52 +0200

    Implementation of blocks' classes front-end side

---
+++ Frontend/Blocks/IFrontEndBlock.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace Frontend.Blocks
+{
+    /// <summary>
+    /// Interfaccia di base per i blocchi lato front-end
+    /// </summary>
+    public interface IFrontEndBlock
+    {
+        /// <summary>
+        /// Forma del blocco
+        /// </summary>
+        public Geometry SvgData { get; set; }
+        
+        /// <summary>
+        /// Elementi contenuti nel blocco
+        /// </summary>
+        public List<IView> Elements { get; set; }
+
+        /// <summary>
+        /// Nome del blocco
+        /// </summary>
+        public string name { get; set; }
+        
+        /// <summary>
+        /// Tipo del blocco
+        /// </summary>
+        public string type { get; set; }
+
+        /// <summary>
+        /// Metodo che permette di ottenere una nuova istanza del blocco
+        /// </summary>
+        /// <returns>una nuova istanza del blocco</returns>
+        public IFrontEndBlock GetNewInstance();
+
+    }
+}
---
+++ Frontend/Blocks/LabelEditor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace Frontend.Blocks
+{
+    /// <summary>
+    /// Classe che rappresenta un blocco con <c>Label</c> e <c>caselle di testo</c>
+    /// </summary>
+    public class LabelEditor : IFrontEndBlock
+    {
+        /// <summary>
+        /// Stringa che rappresenta la forma del blocco
+        /// </summary>
+        private static string _svgData = "m 0,4 A 4,4 0 0,1 4,0 H 12 c 2,0 3,1 4,2 l 4,4 c 1,1 2,2 4,2 h 12 c 2,0 3,-1 4,-2 l 4,-4 c 1,-1 2,-2 4,-2 H 180 a 4,4 0 0,1 4,4 v 40 a 4,4 0 0,1 -4,4 H 48 c -2,0 -3,1 -4,2 l -4,4 c -1,1 -2,2 -4,2 h -12 c -2,0 -3,-1 -4,-2 l -4,-4 c -1,-1 -2,-2 -4,-2 H 4 a 4,4 0 0,1 -4,-4 z";
+        
+        public Geometry SvgData { get; set; }
+
+        /// <summary>
+        /// Lista che conterrà effettivamente gli elementi del blocco
+        /// </summary>
+        private List<IView> _elements { get; set; }
+        public List<IView> Elements {
+            get => _elements;
+            set => _elements = value; 
+        }
+        
+        public string name { get; set; }
+        
+        public string type { get; set; }
+
+
+        /// <summary>
+        /// Costruttore di default della classe
+        /// </summary>
+        public LabelEditor()
+        {
+            this.Elements = new();
+            SvgData = (Geometry)new PathGeometryConverter().ConvertFromInvariantString(_svgData);
+        }
+
+
+        public IFrontEndBlock GetNewInstance()
+        {
+            throw new NotImplementedException("Metodo non implementato.");
+        }
+    }
+}
