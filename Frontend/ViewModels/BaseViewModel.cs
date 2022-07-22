06870813eaf07d0cf1f44d01abf8a87a6cccbac1
Giacomo <giacomo.matteucci@studenti.unicam.it>
2022-07-22 10:00:15 +0200

    Implementation of base class for ViewModels

---
+++ Frontend/ViewModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Frontend.ViewModels
+{
+    /// <summary>
+    /// Classe che rappresenta un ViewModel di base
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Evento che rappresenta il cambiamento di una prorpietà del ViewModel
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Metodo che permette di notificare l'avvenuto cambiamento di una proprietà del ViewModel
+        /// </summary>
+        /// <param name="name"> nome della proprietà del ViewModel che è cambiata </param>
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
