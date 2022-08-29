4ea5b77b71d6dabf46d6ac9fd1fb774fa83e5d12
matias <matiaslarizza@gmail.com>
2022-08-29 22:18:42 +0200

    added translator

---
+++ Frontend/Translators/ITranslator.cs
@@ -0,0 +1,10 @@
+﻿using Backend.Blocks;
+using Frontend.Model.Blocks;
+
+namespace Frontend.Translators
+{
+    public interface ITranslator
+    {
+        List<IBlock> Translate(IEnumerable<IFrontEndBlock> frontEndBlocks);
+    }
+}
