b951f04faae555df21bcaf222d78bf536f3cc980
Leonardo <leonardo.pittori@studenti.unicam.it>
2022-09-06 12:45:06 +0200

    Aggiunta blocchi While-For, e refactoring IfBlock

---
+++ Backend/Blocks/Conditional/WhileBlock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Blocks.Conditional
+{
+    public class WhileBlock : AbstractBlock
+    {
+        private string First { get; set; }
+        private string Second { get; set; }
+        private string Condition { get; set; }
+        public WhileBlock(string name, string first, string condition, string second) : base(name)
+        {
+            First = first;
+            Second = second;
+            Condition = condition;
+        }
+
+        public override string GetCode()
+        {
+            string code = "";
+            code += $"while( ( {First} ) {Condition} ( {Second} ) ){{\n";
+            foreach (var child in Children)
+            {
+                code += child.GetCode();
+            }
+            code += "}\n";
+            return code;
+        }
+
+        public override Dictionary<string, string> GetVariables()
+        {
+            return new();
+        }
+    }
+}
